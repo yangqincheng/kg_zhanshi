e4ae0ec797df45de78fcdc672296e74198e70e05
yangqincheng <yangqcwork@outlook.com>
2020-05-21 00:16:45 +0800

    增加了第三个domain

--- src/main/java/com/warmer/kgmaker/dal/impl/KGraphRepository.java
+++ src/main/java/com/warmer/kgmaker/dal/impl/KGraphRepository.java
@@ -130,6 +130,7 @@ public class KGraphRepository implements IKGraphRepository {
 						nodeSql = String.format("MATCH (m1:`ConceptHyperedge`)-[r1]->(n:Concept)-[r2]->(m2) %s return * limit %s",  cqWhere,
 								query.getPageSize());
 					}
+					// 添加了第三个domain Concept
 					// 下边的查询查不到单个没有关系的节点,考虑要不要左箭头
 					/*String nodeSql = String.format("MATCH (n:`%s`) <-[r]->(m) %s return * limit %s", domain, cqWhere,
 							query.getPageSize());*/
